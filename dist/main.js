2355d6e7bec2f08a446725664fe4df4f038dad80
Jeffrey Ouyang <jeffreyouyang5293@gmail.com>
2015-04-04 22:59:59 -0700

    stickers

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,101 @@
 /*! facebook-chat-plusplus - v0.1.0 - */
 
 var popOverTpl = '\
-   <div class="plus-btn nudge-btn"><a href="#">Nudge</a></div>\
-'
+   <div class="tabs">\
+      <span><a class = "plus-btn nudge-btn" href="#" data-toggle="tab">Nudge</a></span>\
+      <span><a class = "active sticker-btn" href="#" data-toggle="tab">Stickers</a></span>\
+      <span><a class = "encrypt-btn" href="#" data-toggle="tab">Encrypt</a></span>\
+   </div>\
+   <div class="contents">\
+      <div class="tab stickers">\
+      <span><input id="stickerSearch" type="text" placeholder="Search for Stickers"> <button type="button" id="stickerBtn">Search</button></span>\
+      <div id="stickerCont"></div>\
+      </div>\
+      <div class="tab hidden encryption">\
+         Encryption\
+      </div>\
+   </div>\
+   ';
+
+function sendNudge(){
+
+}
+
+function populateStickers(data){
+   var stickersEl = $("#stickerCont");
+   for(var i = 0; i<data['data'].length;i++){
+      stickersEl.append("<img src=\""+data['data'][i]['images']['original']['url']+"\" height=\"50\" width=\"50\">");
+   }
+   $("#stickerCont").on("click", "img", function(){
+      url = $(this).attr('src');
+      $(this).closest('.fbNubFlyoutInner').find('textarea').val(url);
+   });
+}
+trending = [];
+chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
+      switch(message.type) {
+            case "searchResult":
+               $(".loading").remove();
+               console.log("search results:");
+               console.log(message.result);
+               populateStickers(message.result);
+               break;
+            case "trendingResult":
+               $(".loading").remove();
+               console.log("trending results:");
+               console.log(message.result);
+               populateStickers(message.result);
+               break;
+        }
+});
 
 var ChatIconPopoverView = Marionette.LayoutView.extend({
    template: popOverTpl,
+
    events: {
-      'click .plus-btn': 'onNudgeClick'
+      'click .plus-btn': 'onNudgeClick',
+      'click .sticker-btn': 'onStickerClick',
+      'click .encrypt-btn': 'onEncryptClick',
+      'click #stickerBtn' : 'onStickerBtn'
    },
    onNudgeClick: function(){
       console.log("on nudge");
+   },
+
+   onStickerClick: function(){
+      $("#stickerCont").empty();
+      console.log("on sticker");
+      $("#stickerCont").append("<img class=\"loading\" src=\"" + chrome.extension.getURL("icons/spinning-wheel.gif") + "\">");
+      chrome.extension.sendMessage({
+            type: "trending"
+      });
+      if($(".stickers").hasClass('hidden')){
+         $(".stickers").removeClass('hidden');
+      }
+      if(!$(".encryption").hasClass('hidden')){
+         $(".encryption").addClass('hidden');
+      }
+   },
+
+   onStickerBtn: function(){
+      $("#stickerCont").empty();
+      console.log("button clicked");
+      $("#stickerCont").append("<img class=\"loading\" src=\"" + chrome.extension.getURL("icons/spinning-wheel.gif") + "\">");
+      var query = $("#stickerSearch").val();
+      console.log(query);
+      chrome.extension.sendMessage({
+            type: "search",
+            value: query
+      });
+   },
+
+   onEncryptClick: function(){
+      if($(".encryption").hasClass('hidden')){
+         $(".encryption").removeClass('hidden');
+      }
+      if(!$(".stickers").hasClass('hidden')){
+         $(".stickers").addClass('hidden');
+      }
    }
 });
 
@@ -18,7 +103,7 @@ var ChatIconView = Marionette.LayoutView.extend({
    className: 'fb-plusplus-wrap',
    template : '<div class="fb-plusplus-btn-wrap"><img class="fb-plusplus-btn" src="{{icon}}"></div>',
    events: {
-      'click .fb-plusplus-btn' : 'onIconClick'
+      'click .fb-plusplus-btn' : 'onIconClick',
    },
    serializeData: function(){
       return {
@@ -26,15 +111,7 @@ var ChatIconView = Marionette.LayoutView.extend({
       }
    },
    onRender: function(){
-      /*
-      this.$('.fb-plusplus-btn').webuiPopover({
-         title:'Facebook-chat ++',
-         content: function(){
-            return new ChatIconPopoverView().render().el;
-         },
-         placement:'top'
-      });*/
-      console.log(this.$(".fb-plusplus-btn"));
+
       this.$(".fb-plusplus-btn-wrap").popover({
          trigger: 'manual',
          placement: 'top',
